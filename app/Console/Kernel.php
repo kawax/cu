abcd4897bd6b59c523f43a4f16834c1378bd1851
kawax <kawaxbiz@gmail.com>
2020-03-21 19:10:57 +0900

    Update schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(Commands\UpdateCommand::class)
-                 ->dailyAt('19:00');
+                 ->dailyAt('19:30');
 
         $schedule->command('queue:flush')
                  ->dailyAt('5:00');
